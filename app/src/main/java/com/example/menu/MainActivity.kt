0ab2d54eb64b3cf347a67a163bffd4247eda45a8
anathsu <will121212>
2020-03-10 10:59:41 -0300

    Adiciondas opções do menu(Sem redirecionamento)

--- app/src/main/java/com/example/menu/MainActivity.kt
+++ app/src/main/java/com/example/menu/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Add a toolbar como ActionBar no app
+        setSupportActionBar(toolbarSuperior)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return true
     }
 }
