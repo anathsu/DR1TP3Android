5fd0171cc88e5c20d9a233005cb3ba52a9b546b0
anathsu <will121212>
2020-03-10 11:26:08 -0300

    Adiciondas icones no menu e mensagem ao clicar(Sem redirecionamento)

--- app/src/main/java/com/example/menu/MainActivity.kt
+++ app/src/main/java/com/example/menu/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.menu
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +21,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.info_item -> Toast.makeText(this, R.string.informacao, Toast.LENGTH_SHORT).show()
+
+        }
         return true
     }
 }
